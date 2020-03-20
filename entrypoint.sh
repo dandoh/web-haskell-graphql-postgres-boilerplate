1975cd3acd774a0d7a87ea1aab9d20f1e6b10222
Jaime Pascual Torres <cod.al.pro@gmail.com>
2020-03-20 15:02:20 +0100

    Added Dockerfile image from haskell:8.6.5 with dbmate and pgsql, a docker-compose with a daemon entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -x
+
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+# Start PostgreSQL service
+/etc/init.d/postgresql start
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
+
